49f557ba19c31e2d8db7803ee640df34fae50067
pavel-babenko <pavelbabenko.dev@gmail.com>
2021-01-19 14:41:31 +0200

    Refactor API calls to use local storage

--- pb-frontend/src/Routes.tsx
+++ pb-frontend/src/Routes.tsx
@@ -2,7 +2,7 @@ import { Switch, Route } from 'react-router-dom';
 import Pages from './screens/Pages';
 import React, { useEffect } from 'react';
 import usePages from './screens/Pages/hooks/usePages';
-// import Page from './screens/Pages/screens/Page';
+import Page from './screens/Pages/screens/Page';
 import NotFound from './screens/NotFound';
 import { Constructor } from './screens/Constructor';
 
@@ -22,14 +22,14 @@ const Routes = () => {
           component={() => <Constructor _id={_id} />}
         />
       ))}
-      {/*{pages.map(({ _id, route }) => (
+      {pages.map(({ _id, route }) => (
         <Route
           key={route}
           exact
           path={`/${route}`}
           component={() => <Page id={_id} />}
         />
-      ))}*/}
+      ))}
       <Route path="*" component={NotFound} />
     </Switch>
   );
