96a02e38f25bce7dcb86220da6b055ea22190f8c
pavel-babenko <pavelbabenko.dev@gmail.com>
2021-01-21 13:17:00 +0200

    Added component configure modal

--- pb-frontend/src/screens/Constructor/components/Text/index.tsx
+++ pb-frontend/src/screens/Constructor/components/Text/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Input } from './componentsStyles';
+import { Input, TextContainer, Configure } from './componentsStyles';
+import { Modal } from '../Modal';
+import { useModal } from '../../../../hooks/useModal';
 
 type Props = {
   id: string;
@@ -7,12 +9,17 @@ type Props = {
 };
 
 export const Text = ({ id, pointerEventsDisabled }: Props) => {
+  const { modalRef, show } = useModal();
   return (
-    <Input
-      type="text"
-      value={id}
-      readOnly
-      pointerEventsDisabled={pointerEventsDisabled}
-    />
+    <TextContainer>
+      <Input
+        type="text"
+        value={id}
+        readOnly
+        pointerEventsDisabled={pointerEventsDisabled}
+      />
+      <Modal ref={modalRef} id={id} />
+      <Configure onClick={show}>Configure</Configure>
+    </TextContainer>
   );
 };
