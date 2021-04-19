8daa4a4d896a51f80f69fe46367bf757061b1976
Boris <rozumniy.boris.work@gmail.com>
2021-04-19 09:20:10 +0300

    PAG-26: change state by click on the add components button

--- src/components/AddComponents/index.tsx
+++ src/components/AddComponents/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { AddComponentsWrapper } from './componentsStyles';
 
-export const AddComponents = () => {
+type Props = {
+  isOpen: boolean,
+  setIsOpen: (val: boolean) => void,
+};
+
+export const AddComponents: React.FC<Props> = ({
+  isOpen,
+  setIsOpen,
+}) => {
   return (
     <AddComponentsWrapper>
-      <button>+</button>
+      <button onClick={() => setIsOpen(!isOpen)}>{!isOpen ? '+' : 'close'}</button>
       <p>Add components</p>
     </AddComponentsWrapper>
   );
