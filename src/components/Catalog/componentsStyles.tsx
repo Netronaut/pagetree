7d1c920393dae548998e2a03aa54f5e8d1376e29
Boris <rozumniy.boris.work@gmail.com>
2021-04-20 08:21:27 +0300

    PAG-26: changed styles for the DroppableComponentContainer

--- src/components/Catalog/componentsStyles.tsx
+++ src/components/Catalog/componentsStyles.tsx
@@ -1,15 +1,19 @@
 import styled from 'styled-components';
 
 export const DroppableComponentContainer = styled.div({
-  borderRadius: '10px',
-  background: 'white',
-  color: '#282c34',
+  borderRadius: '20px',
+  background: '#69BBFD',
   boxSizing: 'border-box',
   padding: '0 25px',
-  border: '1px dashed',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  margin: '0 10px',
+  margin: '0 0 15px',
   height: '80px',
+  color: '#F9F9F9',
+  fontFamily: 'Gotham Pro',
+  fontSize: '16px',
+  fontWeight: '400',
+  lineHeight: '15px',
+  textAlign: 'center',
 });
