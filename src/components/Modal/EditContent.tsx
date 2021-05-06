24e20641a7215a908a8aa8a8c593325fff7268db
Boris <rozumniy.boris.work@gmail.com>
2021-05-06 07:38:26 +0300

    PAG-26: create the EditContent window

---
+++ src/components/Modal/EditContent.tsx
@@ -0,0 +1,42 @@
+import React, { useContext, useState } from 'react';
+import { Modal } from './index';
+import { ModalButton, ModalInput } from './componentsStyles';
+import { Flex } from '../../componentsStyles';
+
+type Props = {
+  field: string,
+  label: string,
+  type: string,
+  onChange: (e: React.FormEvent<HTMLInputElement>) => void,
+  onSave: any,
+  onCancel: any,
+  onModalClose: () => void,
+  inputValue: string,
+}
+export const EditContent: React.FC<Props> = ({
+  field,
+  label,
+  type,
+  onChange,
+  onSave,
+  onCancel,
+  onModalClose,
+  inputValue,
+}) => {
+  return (
+    <Modal onOpenClose={onModalClose}>
+      <h3>{type}</h3>
+      <ModalInput
+        required
+        placeholder={`Enter ${label}`}
+        value={inputValue}
+        onChange={event => onChange(event)}
+        autoFocus
+      />
+      <Flex mt={16} px={50}>
+        <ModalButton mainStream onClick={onCancel}>Cancel</ModalButton>
+        <ModalButton mainStream whiteBg onClick={() => onSave(field)}>OK</ModalButton>
+      </Flex>
+    </Modal>
+  );
+};
