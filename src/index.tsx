8b52e689f79a5c413229364bc3acb77578120824
Boris <rozumniy.boris.work@gmail.com>
2021-05-13 07:09:51 +0300

    PAG-26: focus on the search input after opening the catalog window

--- src/index.tsx
+++ src/index.tsx
@@ -162,7 +162,13 @@ export const Builder: React.FC<Props> = ({
               <Modal onClose={onModalClose} isAddComponents>
                 <ModalH2>Components</ModalH2>
                 <SearchBox>
-                  <input type="text" placeholder="Search" value={searchValue} onChange={handleSearch} />
+                  <input
+                    type="text"
+                    placeholder="Search"
+                    value={searchValue}
+                    onChange={handleSearch}
+                    autoFocus
+                  />
                   <ModalButton><img src="search.7845d0e5.svg" alt="search" /></ModalButton>
                 </SearchBox>
                 <Catalog searchValue={searchValue} />
