f3c9461d2596d169f87dc9bd45a3fcea7dff1c35
Boris <rozumniy.boris.work@gmail.com>
2021-05-13 11:48:08 +0300

    PAG-26: created the CatalogItem.tsx

--- src/components/Catalog/index.tsx
+++ src/components/Catalog/index.tsx
@@ -1,72 +1,14 @@
-import React, { useState, useContext } from 'react';
-import { StyledGroupWrapper, DropdownButton, StyledCatalogWrapper } from './componentsStyles';
-import { DroppableComponentContainer } from './componentsStyles';
-import { useDragAndDrop } from '../../hooks';
+import React, { useContext } from 'react';
 import { TreeContext } from '../../utils/context';
+import { StyledCatalogWrapper } from './componentsStyles';
+import { CatalogItem } from './CatalogItem';
 import { SearchList } from './SearchList';
 
-const ArrowSvg: React.FC<{ isOpen: boolean }> = ({ isOpen }) => (
-  <svg
-    width='12'
-    height='7'
-    viewBox='0 0 12 7'
-    fill='none'
-    xmlns='http://www.w3.org/2000/svg'
-  >
-    <path
-      d='M1 6L6 1L11 6'
-      stroke={isOpen ? '#6A6A6A' : '#F9F9F9'}
-      strokeLinecap='round'
-      strokeLinejoin='round'
-    />
-  </svg>
-);
-
 type Props = {
-  groupName?: string;
-};
-
-export const CatalogItem: React.FC<Props> = ({ groupName }) => {
-  const { onDragStart } = useDragAndDrop();
-  const [isOpen, setIsOpen] = useState(false);
-  const { components } = useContext(TreeContext);
-
-  const filtered = components?.filter((component) => {
-    const { groupName: groupNameFromFilter } = component;
-    if (groupName === groupNameFromFilter) return component;
-  });
-
-  return (
-    <StyledGroupWrapper isOpen={isOpen}>
-      <header onClick={() => setIsOpen(!isOpen)}>
-        {groupName}
-        <DropdownButton onClick={() => setIsOpen(!isOpen)} isOpen={isOpen}>
-          <ArrowSvg isOpen={isOpen} />
-        </DropdownButton>
-      </header>
-      <section>
-        {filtered?.map((component, i) => (
-          <DroppableComponentContainer
-            id={component.type}
-            key={`droppable-component-${i}`}
-            {...{
-              draggable: true,
-              onDragStart,
-            }}
-          >
-            {component.componentName}
-          </DroppableComponentContainer>
-        ))}
-      </section>
-    </StyledGroupWrapper>
-  );
-};
-
-type CatalogProps = {
   searchValue: string;
-}
+};
 
-export const Catalog: React.FC<CatalogProps> = ({ searchValue }) => {
+export const Catalog: React.FC<Props> = ({ searchValue }) => {
   const { componentGroups } = useContext(TreeContext);
   return (
     <StyledCatalogWrapper>
@@ -76,4 +18,4 @@ export const Catalog: React.FC<CatalogProps> = ({ searchValue }) => {
       )}
     </StyledCatalogWrapper>
   );
-}
+};
