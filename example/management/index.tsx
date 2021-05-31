2e1fa8845416ae4bb4eba350d18255d215551e4b
Boris <rozumniy.boris.work@gmail.com>
2021-05-31 16:19:44 +0300

    PAG-34: corrected navigation

--- example/management/index.tsx
+++ example/management/index.tsx
@@ -1,27 +1,20 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Builder } from '@pagio/builder';
-import { useState } from 'react';
 import { GlobalStyle } from './globalStyle';
-import { components, componentGroups } from './catalog';
-import { Header } from './components/Header';
 import { Route, Router, Switch } from 'react-router';
 import { createBrowserHistory } from 'history';
 import { Nav, StyledLink } from './components/Header/componentsStyles';
 
 const App = () => {
-  const [showPreview, setShowPreview] = useState(false);
-  const [pageContent, setPageContent] = useState({});
   const customHistory = createBrowserHistory();
 
   return (
     <>
       <Router history={customHistory}>
-        <Header setShowPreview={setShowPreview} />
         <Switch>
           <Route path="/pagebuilder">
-            pagebuilder
+            Pagebuilder Page
             {/* <PagebuilderPage /> */}
           </Route>
           <Route path="/management">
@@ -30,8 +23,8 @@ const App = () => {
           </Route>
         </Switch>
         <Nav>
-          <StyledLink to="/pagebuilder">Home</StyledLink>
-          <StyledLink to="/management">Create Article</StyledLink>
+          <StyledLink to="/pagebuilder">Pagebuilder</StyledLink>
+          <StyledLink to="/management">Management</StyledLink>
         </Nav>
       </Router>
       <GlobalStyle />
