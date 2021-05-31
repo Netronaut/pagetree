0851013de5fd591eb970943c43d4be282d8be37c
Boris <rozumniy.boris.work@gmail.com>
2021-05-31 13:34:10 +0300

    PAG-34: recreate management directory

---
+++ example/management/catalog/article-teaser.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+import {
+  createCatalogComponent,
+  ProductionComponentProps,
+} from '@pagio/builder';
+
+const Container = styled.div`
+  box-sizing: border-box;
+  outline: none;
+  height: 200px;
+  width: 100%;
+`;
+
+const ProductionContainer = styled.div`
+  background: no-repeat url('https://picsum.photos/480/600');
+  background-size: cover;
+  height: 200px;
+  color: white;
+  font-size: 24px;
+  display: flex;
+  align-items: flex-end;
+  padding: 20px;
+`;
+
+const Production: React.FC<ProductionComponentProps> = ({ config }) => {
+  return <ProductionContainer>{config?.articleId}</ProductionContainer>;
+};
+
+export default createCatalogComponent(Container, Production, {
+  type: 'article-teaser',
+  componentName: 'Article Teaser',
+  groupName: 'Component group 1',
+  props: [{ fieldName: 'articleId', label: 'articleId' }],
+});
