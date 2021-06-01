b7622cb138988e40dec39e2ec16da79e2a193092
Boris <rozumniy.boris.work@gmail.com>
2021-06-01 11:44:32 +0300

    PAG-34: add the createUrlFromText fanction

--- example/management/components/ArticlePage/index.tsx
+++ example/management/components/ArticlePage/index.tsx
@@ -6,15 +6,35 @@ type Props = {
 
 export const ArticlePage: React.FC<Props> = ({ prop }) => {
   const [value, setValue] = useState('');
+  const [link, setLInk] = useState('');
+
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
     const { value } = e.currentTarget;
     setValue(value);
   };
+
+  const createUrlFromText = (text: string) => {
+    return (
+      '/' +
+      text
+        .split('')
+        .map(littera => {
+          if (littera === ' ') return '-';
+          return littera;
+        })
+        .join('')
+        .toLowerCase()
+    );
+  };
+
   return (
     <>
       <h1>Create Article</h1>
       <input onChange={handleChange} />
-      <h3>{value}</h3>
+      <button onClick={() => setLInk(createUrlFromText(value))}>
+        create link
+      </button>
+      <h3>{link}</h3>
     </>
   );
 };
