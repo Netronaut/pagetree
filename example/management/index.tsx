e2472806ab4255cbfdf94a19e6dca39886195d64
Boris <rozumniy.boris.work@gmail.com>
2021-06-04 06:12:46 +0300

    PAG-34: added the ManagementContext; used react-router-dom

--- example/management/index.tsx
+++ example/management/index.tsx
@@ -1,19 +1,35 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { useCallback, useState, useEffect } from 'react';
+import axios from 'axios';
 import { GlobalStyle } from './globalStyle';
-import { Route, Router, Switch } from 'react-router';
-import { createBrowserHistory } from 'history';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Nav, StyledLink } from './components/ArticlePage/componentsStyles';
 import { ArticlePage } from './components/ArticlePage';
 import { PagebuilderPage } from './components/PagebuilderPage';
+import { ManagementContext } from './utils/context';
+import { apiUrls } from './apiUrls';
 
 const App = () => {
-  const customHistory = createBrowserHistory();
+  const [articles, setArticles] = useState([]);
+
+  const getArticles = useCallback(async () => {
+    try {
+      const response = await axios.get(apiUrls.aricles);
+      setArticles(response.data);
+    } catch (error) {
+      setArticles(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getArticles();
+  }, []);
 
   return (
-    <>
-      <Router history={customHistory}>
+    <ManagementContext.Provider value={{ articles }}>
+      <Router>
         <Switch>
           <Route path="/management">
             <ArticlePage />
@@ -24,11 +40,10 @@ const App = () => {
         </Switch>
         <Nav>
           <StyledLink to="/management">Management</StyledLink>
-          <StyledLink to="/pagebuilder">Pagebuilder</StyledLink>
         </Nav>
       </Router>
       <GlobalStyle />
-    </>
+    </ManagementContext.Provider>
   );
 };
 
