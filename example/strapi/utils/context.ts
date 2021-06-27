d88c7001bd2d5c3cfbb65e909beca2c3b7c92d5f
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2021-06-27 23:46:53 +0200

    fix hooks issues, dedupe react, fix more typing issues

--- example/strapi/utils/context.ts
+++ example/strapi/utils/context.ts
@@ -1,11 +1,9 @@
-import { Dispatch } from 'react';
-import { SetStateAction } from 'react';
 import { createContext } from 'react';
 import { TPageData } from '../types';
 
 type ManagementContextValue = {
   pages: TPageData[];
-  changePages: Dispatch<SetStateAction<never[]>>;
+  changePages: (pageData: TPageData[]) => void;
 };
 
 export const ManagementContext = createContext<ManagementContextValue>({
