66579a9ce6cfdf93538811ce19c266fac582cf21
Jakob Hohlfeld <jakob.hohlfeld@cbc-extern.de>
2021-07-07 19:12:15 +0200

    refactor(PAG-57): refactor Builder.tsx, rename TPage interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,9 @@
 import { ChildDirection } from '../utils/tree';
 
-export type TBasePage = {
-  title: string;
-  route: string;
-};
+export interface PageStructure {
+  _id: string;
+  structure?: ChildDirection;
+  config?: Record<string, Record<string, string>>;
+}
 
-export type TPage =
-  | TBasePage & {
-      _id: string;
-      structure?: ChildDirection;
-      config?: Record<string, Record<string, string>>;
-    };
-
-export type TPages = TPage[];
+export type PageStructures = PageStructure[];
--- src/utils/context.ts
+++ src/utils/context.ts
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { TSide } from './tree';
-import { TPage } from '../types';
+import { PageStructure } from '../types';
 import { Components } from '../hocs/createCatalogComponent';
 
 type TreeContextValue = {
   add: (e: React.DragEvent<HTMLDivElement>, toId?: string, side?: TSide) => void;
   onConfigChange: (id: string, field: string, value: string, userControlledId?: string) => void;
-  config: TPage['config'];
+  config: PageStructure['config'];
   showPreview?: boolean;
   components?: Components;
   componentGroups?: string[];
