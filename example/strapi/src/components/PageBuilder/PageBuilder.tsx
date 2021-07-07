f68d915dedc93e1ecc266ad1a4ca4cb207a0216a
Jakob Hohlfeld <jakob.hohlfeld@cbc-extern.de>
2021-07-07 19:17:16 +0200

    refactor(PAG-57): refactor PageBuilder: move data handling to container component

--- example/strapi/src/components/PageBuilder/PageBuilder.tsx
+++ example/strapi/src/components/PageBuilder/PageBuilder.tsx
@@ -1,58 +1,32 @@
-import React, { ReactElement, useState, useContext, useEffect } from 'react';
-import { match } from 'react-router';
-import { Builder, TPage } from '@pagio/builder';
-import axios from 'axios';
+import React, { ReactElement, useContext, useMemo } from 'react';
+import { useParams } from 'react-router';
+import { Builder } from '@pagio/builder';
 import { components, componentGroups } from '../../catalog';
 import { ManagementContext } from '../../utils/context';
-import { apiUrls } from '../../apiUrls';
-import { TPageData } from '../../types';
-import { Header } from '../Header';
+import { PageEntity } from '../../types';
 
 interface PageBuilderProps {
-  match: match<{ pageid: string }>;
+  showPreview: boolean;
+  onPageUpdate: (page: PageEntity) => void;
 }
 
-export const PageBuilder = ({ match }: PageBuilderProps): ReactElement => {
-  const { pageid } = match.params;
+export const PageBuilder = ({ showPreview, onPageUpdate }: PageBuilderProps): ReactElement => {
+  const { pageId } = useParams<{ pageId: string }>();
+  const { pages } = useContext(ManagementContext);
 
-  const [showPreview, setShowPreview] = useState(false);
-  const { pages, setPages } = useContext(ManagementContext);
-  const [page, setPage] = useState<TPageData | undefined>({} as TPageData);
-  const [pageContent, setPageContent] = useState<TPage | undefined>({} as TPage);
+  const page =
+    useMemo(() => pages.find(({ id }) => String(id) === pageId), [pageId, pages]) ||
+    ({} as PageEntity);
 
-  useEffect(() => {
-    const currentPage = pages.find((page) => String(page.id) === pageid);
-    setPage(currentPage);
-    currentPage?.pageContent && setPageContent(currentPage?.pageContent);
-  }, [pages]);
-
-  useEffect(() => {
-    JSON.stringify(page?.pageContent) !== JSON.stringify(pageContent) &&
-      page?.id &&
-      axios
-        .put(`${apiUrls.pages}/${page?.id}`, {
-          pageContent,
-        })
-        .then((response) => {
-          const copyPages = pages.slice();
-          const findedIndex = copyPages.findIndex((page) => page.id === response.data.id);
-          copyPages.splice(findedIndex, 1, response.data);
-          setPages(copyPages);
-        });
-  }, [pageContent]);
+  const { pageContent } = page;
 
   return (
-    <>
-      <Header setShowPreview={setShowPreview} />
-      {pageContent && (
-        <Builder
-          pageContent={pageContent}
-          onChange={setPageContent}
-          showPreview={showPreview}
-          components={components}
-          componentGroups={componentGroups}
-        />
-      )}
-    </>
+    <Builder
+      pageContent={pageContent}
+      onChange={(pageContentUpdate) => onPageUpdate({ ...page, pageContent: pageContentUpdate })}
+      showPreview={showPreview}
+      components={components}
+      componentGroups={componentGroups}
+    />
   );
 };
--- example/strapi/src/index.tsx
+++ example/strapi/src/index.tsx
@@ -1,34 +1,37 @@
-import React, { useCallback, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import axios from 'axios';
+
 import { GlobalStyle } from './globalStyle';
 import { PageBuilder, PageManager } from './components';
 import { ManagementContext } from './utils/context';
 import { apiUrls } from './apiUrls';
-import { TPageData } from './types';
+import { PageEntity } from './types';
 
 const App = () => {
-  const [pages, setPages] = useState<TPageData[]>([]);
-
-  const getPages = useCallback(async () => {
-    try {
-      const response = await axios.get(apiUrls.pages);
-      setPages(response.data);
-    } catch (error) {
-      setPages(error);
-    }
-  }, []);
+  const [pages, setPages] = useState<PageEntity[]>([]);
+  const [showPreview, setShowPreview] = useState(false);
 
   useEffect(() => {
-    getPages();
+    axios.get(apiUrls.pages).then((response) => setPages(response.data));
   }, []);
+
+  const handlePageUpdate = (page: PageEntity) => {
+    const { id, pageContent } = page;
+    axios.put(`${apiUrls.pages}/${id}`, {
+      pageContent,
+    });
+    setPages(pages.map((item) => (item.id === id ? page : item)));
+  };
 
   return (
     <ManagementContext.Provider value={{ pages, setPages }}>
       <Router>
         <Switch>
-          <Route path="/pagebuilder/:pageid" component={PageBuilder} />
+          <Route path="/pagebuilder/:pageId">
+            <PageBuilder showPreview={showPreview} onPageUpdate={handlePageUpdate} />
+          </Route>
           <Route path="/" exact component={PageManager} />
           <Route path="*">
             <Redirect to="/" />
