acc7cb0f32eeb7c6fb5c51f1d4cc5dab8205ee10
Boris <rozumniy.boris.work@gmail.com>
2021-07-13 21:05:27 +0300

    chore(PAG-59): added the HistoryLogItem and the Change types

--- example/strapi/src/types.ts
+++ example/strapi/src/types.ts
@@ -1,4 +1,5 @@
 import { PageStructure } from '@pagio/builder/dist/types';
+import { ProductionComponentProps } from '@pagio/builder';
 
 export interface PageEntity {
   id?: number;
@@ -6,3 +7,14 @@ export interface PageEntity {
   path: string;
   pageContent?: PageStructure;
 }
+
+export type HistoryLogItem = {
+  date: string;
+  change: Change[];
+};
+
+export type Change = {
+  type: string;
+  key: string[];
+  value: string | ProductionComponentProps;
+};
