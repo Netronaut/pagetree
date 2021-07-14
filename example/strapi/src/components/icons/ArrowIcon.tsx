e7f1b77f9d7ef40f34a8206614826a5965f46cbe
Boris <rozumniy.boris.work@gmail.com>
2021-07-14 17:37:56 +0300

    chore(PAG-59): removed black color from the ArrowIcon

--- example/strapi/src/components/icons/ArrowIcon.tsx
+++ example/strapi/src/components/icons/ArrowIcon.tsx
@@ -1,16 +1,7 @@
 import React, { ReactElement } from 'react';
 
-export interface ArrowIconProps {
-  isOpen?: boolean;
-}
-
-export const ArrowIcon = ({ isOpen }: ArrowIconProps): ReactElement => (
+export const ArrowIcon = (): ReactElement => (
   <svg width="18" height="8" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M1 6L6 1L11 6"
-      stroke={isOpen ? '#6A6A6A' : '#F9F9F9'}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
+    <path d="M1 6L6 1L11 6" stroke="#F9F9F9" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 );
