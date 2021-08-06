a0ed32b12c5e4cd39bde495f3a8f96372b647778
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2021-08-06 13:55:25 +0200

    feature(PAG-46): add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: [
+    '../example/**/src/**/*.stories.mdx',
+    '../example/**/src/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials'
+  ],
+};
