334631896a1363c96c559af04ee3b0092a8a6a56
Boris <boris.razumny@flair.dev>
2021-08-13 02:32:47 +0300

    feture(PAG-46): described the SearchIcon in the storybook

---
+++ example/dashboard/src/icons/SearchIcon.stories.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+import { SearchIcon } from './SearchIcon';
+
+export const Search = (): ReactElement => <SearchIcon />;
+
+export default {
+  title: 'Icons/SearchIcon',
+  component: SearchIcon,
+};
