4a387832c701b54ea5b860e5372be4e652476b61
Boris <boris.razumny@flair.dev>
2021-08-13 11:27:21 +0300

    chore(PAG-46): corrected svg attributes in the SearchIcon

--- example/dashboard/src/icons/SearchIcon.tsx
+++ example/dashboard/src/icons/SearchIcon.tsx
@@ -9,16 +9,16 @@ export const SearchIcon: React.FC<SearchIconProps> = ({ isOutlined }) => (
     <path
       d="M13.1743 15.6364C9.74531 15.6364 6.96552 12.7873 6.96552 9.27275C6.96552 5.75822 9.74531 2.90912 13.1743 2.90912C16.6034 2.90912 19.3832 5.75822 19.3832 9.27275C19.3832 12.7873 16.6034 15.6364 13.1743 15.6364Z"
       stroke={isOutlined ? '#39A7FF' : '#AAAAAA'}
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M3.41759 19.2727L8.73944 13.8182"
       stroke={isOutlined ? '#39A7FF' : '#AAAAAA'}
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
