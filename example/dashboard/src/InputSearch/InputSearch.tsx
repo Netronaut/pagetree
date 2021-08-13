c707b01ca5cfb8e99ccc7fece94de2c999e103f2
Boris <boris.razumny@flair.dev>
2021-08-13 10:40:36 +0300

    feture(PAG-46): Visualized the InputSearch highlighting

--- example/dashboard/src/InputSearch/InputSearch.tsx
+++ example/dashboard/src/InputSearch/InputSearch.tsx
@@ -1,10 +1,13 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import { SearchIcon } from '../icons';
 import { SearchBox } from './InputSearch.styles';
 
-export const InputSearch = (): ReactElement => (
-  <SearchBox>
-    <input placeholder="Search" />
-    <SearchIcon />
-  </SearchBox>
-);
+export const InputSearch = (): ReactElement => {
+  const [isSelected, setIsSelected] = useState(false);
+  return (
+    <SearchBox onSelect={() => setIsSelected(true)} onBlur={() => setIsSelected(false)}>
+      <input placeholder="Search" />
+      <SearchIcon isOutlined={isSelected} />
+    </SearchBox>
+  );
+};
