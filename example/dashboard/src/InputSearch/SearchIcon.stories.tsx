7251b60f2e3cc624c32041cbcb65697096ff87a0
Boris <boris.razumny@flair.dev>
2021-08-13 10:28:01 +0300

    feture(PAG-46): described the InputSearch in the storybook

---
+++ example/dashboard/src/InputSearch/SearchIcon.stories.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+import { InputSearch } from './InputSearch';
+
+export default {
+  title: 'Example/InputSearch',
+  component: InputSearch,
+};
+
+export const pageList = (): ReactElement => <InputSearch />;
