09fd0f2fe05c298d9b8ce229b780e40b53a2d904
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2021-08-14 14:06:52 +0200

    feature(PAG-77): add PageList

---
+++ example/dashboard/src/PageList/PageItem.tsx
@@ -0,0 +1,71 @@
+import React, { ReactElement, useState } from 'react';
+import { PageEntity } from '../../../strapi/src/types';
+import { DeleteIcon, StarIcon, EditIcon, IconButton } from '../icons';
+import { Tooltip } from '../Tooltip';
+import { Larger, Smaller } from '../Typography';
+import { QuickActionGrid } from './PageList.styles';
+
+interface PageItemProps {
+  page: PageEntity;
+  onEdit: (page: PageEntity) => void;
+  onRemove: (page: PageEntity) => void;
+  onSelect: (page: PageEntity) => void;
+}
+
+export const PageItem = ({ page, onSelect, onEdit, onRemove }: PageItemProps): ReactElement => {
+  const { title, version } = page;
+  const [quickActionsVisible, setQuickActionsVisible] = useState(false);
+  return (
+    <tr
+      onMouseOver={() => setQuickActionsVisible(true)}
+      onMouseOut={() => setQuickActionsVisible(false)}
+      onClick={() => onSelect(page)}
+    >
+      <td>
+        <Tooltip content="Pin page">
+          <IconButton
+            onClick={(event) => {
+              event.stopPropagation();
+              onEdit({ ...page, starred: !page.starred });
+            }}
+          >
+            <StarIcon fill={page.starred} />
+          </IconButton>
+        </Tooltip>
+      </td>
+      <td>
+        <Larger>{title}</Larger>
+      </td>
+      <td>
+        <QuickActionGrid
+          visible={quickActionsVisible}
+          onMouseOver={() => setQuickActionsVisible(true)}
+        >
+          <Tooltip content={<span>Edit page</span>}>
+            <IconButton
+              onClick={(event) => {
+                event.stopPropagation();
+                onEdit(page);
+              }}
+            >
+              <EditIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip content={<span>Remove page</span>}>
+            <IconButton
+              onClick={(event) => {
+                event.stopPropagation();
+                onRemove(page);
+              }}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+        </QuickActionGrid>
+      </td>
+      <td>
+        <Smaller>{version}</Smaller>
+      </td>
+    </tr>
+  );
+};
