80d64c3d17b8d4155f0eec431aadc8edf7320084
Boris <boris.razumny@flair.dev>
2021-08-20 16:16:32 +0300

    feature(PAG-78): Created the Edit Modal with styles and storybook

---
+++ example/dashboard/src/icons/CloseIcon.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from 'react';
+
+export const CloseIcon = (): ReactElement => (
+  <svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <line x1="17.3536" y1="0.353553" x2="1.35355" y2="16.3536" stroke="black" />
+    <line
+      y1="-0.5"
+      x2="22.6274"
+      y2="-0.5"
+      transform="matrix(0.707107 0.707107 0.707107 -0.707107 1 0)"
+      stroke="black"
+    />
+  </svg>
+);
