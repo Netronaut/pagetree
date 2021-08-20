aadf8dafcb72ad806fc57fe6e74fa916087f7a69
Boris <boris.razumny@flair.dev>
2021-08-20 16:16:32 +0300

    feature(PAG-78): Created the Edit Modal with styles and storybook

---
+++ example/dashboard/src/Modal/Modal.tsx
@@ -0,0 +1,80 @@
+import React, {
+  ChangeEvent,
+  ReactElement,
+  useRef,
+  useState,
+  KeyboardEvent,
+  MouseEventHandler,
+} from 'react';
+import { PageEntity } from '../../../strapi/src/types';
+import { useTapOutside } from '../../../strapi/src/components/PageManager/hooks';
+import { ModalContainer } from './Modal.styles';
+import { CloseIcon } from '../icons';
+import { Button } from '../Button';
+import { LargerMedium, SmallerBold, Smaller } from '../Typography';
+
+interface ModalProps {
+  page: PageEntity;
+  onClose: () => MouseEventHandler;
+  onSave: (page: PageEntity) => void;
+}
+
+export const Modal = ({ onClose, onSave, ...props }: ModalProps): ReactElement => {
+  const [page, setPage] = useState({ ...props.page });
+
+  const wrapperRef = useRef(null);
+  useTapOutside(wrapperRef, onClose);
+
+  const handleChange = (e: KeyboardEvent<HTMLInputElement> | ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.currentTarget;
+
+    if ((e as KeyboardEvent<HTMLInputElement>).key === 'Enter' && page.title !== '') {
+      return onSave(page);
+    }
+
+    if ((e as KeyboardEvent<HTMLInputElement>).key === 'Escape') {
+      return onClose();
+    }
+
+    setPage({ ...page, [name]: value });
+  };
+
+  return (
+    <ModalContainer ref={wrapperRef} data-testid="edit-modal">
+      <button onClick={onClose}>
+        <CloseIcon />
+      </button>
+      <LargerMedium>Edit Page</LargerMedium>
+      <label>
+        <SmallerBold>Page title</SmallerBold>
+        <input
+          autoFocus
+          type="text"
+          name="title"
+          placeholder="The title of your page"
+          data-testid="edit-input"
+          defaultValue={page.title}
+          onChange={handleChange}
+          onKeyDown={handleChange}
+        />
+        <Smaller>Add a title identifiying your page in the page builder</Smaller>
+      </label>
+      <label>
+        <SmallerBold>Page title</SmallerBold>
+        <input
+          type="text"
+          name="path"
+          placeholder="The path of your page"
+          data-testid="edit-input"
+          defaultValue={page.path}
+          onChange={handleChange}
+          onKeyDown={handleChange}
+        />
+        <Smaller>Add a path relative to your base URL</Smaller>
+      </label>
+      <Button secondary onClick={onSave}>
+        Save
+      </Button>
+    </ModalContainer>
+  );
+};
