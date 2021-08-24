d6374fbd52afc771eb0989b303caa09a5db9c576
herrhelms <helms@straw.io>
2021-08-24 14:07:05 +0200

    feature(PAG-79): add spacing to theme

--- example/dashboard/src/theme.ts
+++ example/dashboard/src/theme.ts
@@ -8,3 +8,21 @@ export const color = {
   gray3: '#d0d0d0',
   gray4: '#f5f5f5',
 };
+
+export const spacing = {
+  vertical: {
+    small: '0 1em',
+    medium: '0 1.375em',
+    large: '0 2.75em',
+  },
+  horizontal: {
+    small: '1em 0',
+    medium: '1.375em 0',
+    large: '2.75em 0',
+  },
+  any: {
+    small: '1em',
+    medium: '1.375em',
+    large: '2.75em',
+  },
+};
