9d4444f4d7e3373365c1ecf0db16b114de665247
Boris <boris.razumny@flair.dev>
2021-08-25 10:05:48 +0300

    feateure(PAG-78): create the ModalInput

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import { ThemeProvider } from 'styled-components';
 import * as theme from '../example/dashboard/src/theme.ts';
+import { GlobalStyle } from '../example/dashboard/src/globalStyle';
 import './style.css';
 
 export const parameters = {
@@ -15,6 +16,7 @@ export const parameters = {
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Story />
     </ThemeProvider>
   ),
