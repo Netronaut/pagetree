d18a0414de01b8da4371cdea682e0cd9c31065d5
herrhelms <helms@straw.io>
2021-08-26 22:14:05 +0200

    feature(PAG-82): add AddIcon for component catalog

--- example/dashboard/src/icons/index.ts
+++ example/dashboard/src/icons/index.ts
@@ -1,6 +1,7 @@
 export * from './DeleteIcon';
 export * from './EditIcon';
 export * from './StarIcon';
+export * from './AddIcon';
 export * from './SearchIcon';
 export * from './IconButton';
 export * from './CloseIcon';
