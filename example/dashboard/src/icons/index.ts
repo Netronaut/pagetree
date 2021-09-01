18a952fe640d82c71e022f9ba96b5fc64139d7c2
herrhelms <helms@straw.io>
2021-09-01 13:35:36 +0200

    fix(PAG-82): remove obsolete AddIcon

--- example/dashboard/src/icons/index.ts
+++ example/dashboard/src/icons/index.ts
@@ -1,7 +1,6 @@
 export * from './DeleteIcon';
 export * from './EditIcon';
 export * from './StarIcon';
-export * from './AddIcon';
 export * from './SearchIcon';
 export * from './IconButton';
 export * from './CloseIcon';
