7452e26d0b17b12170637eee6e74daa149b66458
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2021-09-05 11:00:01 +0200

    feat(PAG-82): add Catalog component

---
+++ example/dashboard/src/Catalog/Catalog.styles.tsx
@@ -0,0 +1,146 @@
+import styled from 'styled-components';
+import { Default } from '../Typography';
+
+export const CatalogRoot = styled.div<{ expanded: boolean }>`
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  border-radius: 4px;
+  background-color: ${({ theme }) => theme.color.white};
+  box-shadow: 0px 4px 8px 0px #13592220;
+
+  user-select: none;
+
+  ${({ expanded, theme }) => `
+    padding: ${theme.spacing.xs} ${theme.spacing.xs};
+    background: ${theme.color.white};
+    right: ${theme.spacing.md};
+    bottom: ${theme.spacing.lg};
+
+    ${
+      expanded &&
+      `
+      cursor: pointer;
+      left: ${theme.spacing.md};
+      padding: ${theme.spacing.sm};
+    `
+    }
+  `}
+`;
+
+export const CatalogHeader = styled.div<{ expanded: boolean }>`
+  display: grid;
+  align-items: center;
+
+  ${({ expanded, theme }) =>
+    expanded &&
+    `
+      gap: ${theme.spacing.md};
+      grid-template-columns: 1fr 1fr 0fr;
+    `}
+`;
+
+export const CatalogBody = styled.div`
+  display: flex;
+  width: 100%;
+  height: 130px;
+  gap: ${({ theme }) => theme.spacing.md};
+  overflow-x: auto;
+  margin-top: ${({ theme }) => theme.spacing.md};
+
+  ::-webkit-scrollbar {
+    height: 6px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: ${({ theme }) => theme.color.secondary};
+    border-radius: 3px;
+  }
+`;
+
+export const CatalogTags = styled.div<{ expanded: boolean }>`
+  display: grid;
+  grid-auto-flow: column;
+  gap: ${({ theme }) => theme.spacing.xxs};
+  overflow: hidden;
+  justify-self: start;
+`;
+
+export const CatalogItem = styled(Default).attrs(() => ({ as: 'div' }))`
+  display: flex;
+  position: relative;
+  min-width: 180px;
+  max-width: 180px;
+  height: 110px;
+  padding: 0.5em;
+  cursor: grab;
+  flex-direction: column;
+  justify-content: end;
+  align-items: center;
+  border-radius: 4px;
+  white-space: nowrap;
+
+  user-select: none;
+
+  ${({ theme }) => `
+    border: 1px solid ${theme.color.gray3};
+    color: ${theme.color.gray3};
+  `}
+
+  :after {
+    content: '';
+    border-top: 1px solid ${({ theme }) => theme.color.gray3};
+    position: absolute;
+    bottom: 2.2em;
+    width: 180px;
+  }
+
+  :hover {
+    ${({ theme }) => `
+      border-color: ${theme.color.secondary};
+      color: ${theme.color.secondary};
+
+      :after {
+        border-color: ${theme.color.secondary};
+      }
+    `}
+  }
+`;
+
+export const CatalogOpenCloseButton = styled.button<{ expanded: boolean }>`
+  display: grid;
+  grid-auto-flow: column;
+  gap: 0.5rem;
+  align-items: center;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  white-space: nowrap;
+  padding: 0;
+
+  ${({ expanded, theme }) =>
+    expanded
+      ? `
+    span {
+      display: none;
+    }
+
+    svg path {
+      stroke: ${theme.color.gray2};
+    }
+
+    :hover svg path {
+      stroke: ${theme.color.secondary};
+    }
+    `
+      : `
+    svg {
+      transform: scale(0.75) rotate(45deg);
+
+      path {
+        stroke: ${theme.color.secondary};
+        stroke-width: 3;
+      }
+    }
+  `}
+`;
---
+++ example/dashboard/src/Catalog/__mocks__/components.ts
@@ -0,0 +1,29 @@
+import { CatalogComponentDescription } from '@pagio/builder';
+
+export const components = [
+  {
+    type: 'article-teaser',
+    label: 'Article Teaser',
+    tags: ['article'],
+  },
+  {
+    type: 'headline',
+    label: 'Headline',
+    tags: ['headline'],
+  },
+  {
+    type: 'another-teaser',
+    label: 'Another Teaser',
+    tags: ['article'],
+  },
+  {
+    type: 'test-component',
+    label: 'Test Component',
+    tags: ['test'],
+  },
+  {
+    type: 'test-component-2',
+    label: 'Test Component 2',
+    tags: ['test'],
+  },
+] as Array<CatalogComponentDescription>;
