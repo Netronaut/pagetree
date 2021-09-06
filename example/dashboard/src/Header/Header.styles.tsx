dba0ba26e87e67f87fe9119489a18bd2b6aec700
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2021-09-06 21:41:35 +0200

    feature(PAG-80): add Header

---
+++ example/dashboard/src/Header/Header.styles.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import { Button } from '../Button';
+import { IconButton } from '../icons';
+
+export const HeaderRoot = styled.section`
+  display: grid;
+  grid-template-columns: auto auto 1fr auto auto;
+  grid-column-gap: ${({ theme }) => theme.spacing.xxs};
+  ${IconButton} {
+    padding: ${({ theme }) => theme.spacing.xs};
+    border-right: 1px solid ${({ theme }) => theme.color.gray4};
+    height: 100%;
+    :last-child {
+      border-right: none;
+    }
+  }
+`;
+
+export const HeaderGroup = styled.div<{ padding?: string; columnNumber?: number }>`
+  display: flex;
+  background: ${({ theme }) => theme.color.white};
+  box-shadow: 0px 4px 8px 0px #13592233;
+  border-radius: 4px;
+  ${({ padding, theme }) => padding && `padding: ${theme.spacing[padding]}`};
+  ${({ columnNumber }) => columnNumber && `grid-column: ${columnNumber}`};
+  ${Button} {
+    min-width: 134px;
+    line-height: 15px;
+    font-size: 13px;
+  }
+`;
+
+export const TextGroup = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  padding-left: ${({ theme }) => theme.spacing.xxs};
+  ${IconButton} {
+    padding-right: ${({ theme }) => theme.spacing.xs};
+    padding-left: ${({ theme }) => theme.spacing.xl};
+    :last-child {
+      border-right: 1px solid ${({ theme }) => theme.color.gray4};
+    }
+  }
+`;
