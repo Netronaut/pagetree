bbe363e836631acd6fad46963bc99403222ee3e3
Boris <boris.razumny@flair.dev>
2021-09-17 11:51:09 +0300

    feature(PAG-62): created the LogItem component with some mockData

---
+++ packages/components/src/Changelog/LogItem.tsx
@@ -0,0 +1,45 @@
+import React, { ReactElement, useState } from 'react';
+import { SmallerBold } from '../Typography';
+import {
+  LogItemRoot,
+  SubList,
+  SubListItem,
+  Text,
+  TextGray,
+  TextCapitalized,
+} from './Changelog.styles';
+
+interface LogItemProps {
+  logItem: {
+    id: number;
+    title: string;
+    publishedDate: string;
+    details: {
+      id: number;
+      data: string;
+      changes: number;
+    }[];
+  };
+  selected: boolean;
+}
+
+export const LogItem = ({ logItem, selected }: LogItemProps): ReactElement => {
+  const [openedSubList, setOpenedSubList] = useState(selected || false);
+
+  return (
+    <LogItemRoot onClick={() => setOpenedSubList(!openedSubList)} selected={selected}>
+      <SmallerBold>{logItem.title}</SmallerBold>
+      <TextCapitalized>{logItem.publishedDate}</TextCapitalized>
+      {openedSubList && (
+        <SubList>
+          {logItem.details.map((detail) => (
+            <SubListItem key={detail.id}>
+              <TextGray>{detail.data}</TextGray>
+              <Text>{detail.changes} changes</Text>
+            </SubListItem>
+          ))}
+        </SubList>
+      )}
+    </LogItemRoot>
+  );
+};
