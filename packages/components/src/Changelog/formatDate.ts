af3306db471fca0242fccc9382903dfdd4a20bfe
Boris <boris.razumny@flair.dev>
2021-10-04 13:08:59 +0300

    feature(PAG-62): created a test for the formatDate function; corrected the formatDate

--- packages/components/src/Changelog/formatDate.ts
+++ packages/components/src/Changelog/formatDate.ts
@@ -1,14 +1,14 @@
-export const formatDate = (isoDate: string, format: string): string => {
+export const formatDate = (isoDate: string, format?: string): string => {
   const d = new Date(isoDate);
   const [day, month, date, year, time] = String(new Date(isoDate)).split(' ');
-  const MM = d.getMonth() < 10 ? '0' + d.getMonth() : d.getMonth();
+  const MM = d.getMonth() < 9 ? `0${d.getMonth() + 1}` : d.getMonth() + 1;
 
   if (format === 'DDD, dd MMM yyyy hh:mm:ss') {
     return `${day.toUpperCase()}, ${date} ${month} ${year} ${time}`;
   }
 
   if (format === 'dd.mm.yy') {
-    return `${date}.${MM}.${String(d.getFullYear()).slice(2)}`;
+    return `${date}.${MM}.${String(year).slice(2)}`;
   }
 
   return [day, month, date, year, time].join(' ');
