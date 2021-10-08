ff14a9709b0474247ac21b6a32e870c43ccee7ad
Boris <boris.razumny@flair.dev>
2021-10-08 12:44:29 +0300

    feature(PAG-62): created the historyToVersions function

--- packages/components/src/Changelog/Changelog.tsx
+++ packages/components/src/Changelog/Changelog.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement, useMemo, useState } from 'react';
 import { PageEntity, PageHistory } from '../types';
 import { LogList } from './Changelog.styles';
 import { LogItem } from './LogItem';
+import { historyToVersions } from './historyToVersions';
 
 interface ChangelogProps {
   page: PageEntity;
@@ -11,14 +12,11 @@ type VersionedPageHistory = Record<string, PageHistory>;
 
 export const Changelog = ({ page }: ChangelogProps): ReactElement => {
   const changesByVersion = useMemo(() => {
-    let version = '<unversioned>';
-    return page.history?.reduce((versions, historyItem) => {
-      version = historyItem.version || version;
-      return { ...versions, [version]: (versions[version] || []).concat(historyItem) };
-    }, {} as VersionedPageHistory);
+    if (page.history) return historyToVersions(page.history);
+    return {};
   }, [page.history]) as VersionedPageHistory;
 
-  const [currentVersionId] = useState<string>(Object.keys(changesByVersion)[0]);
+  const [currentVersionId] = useState<string>(Object.keys(changesByVersion)[0] || '');
   return (
     <LogList>
       {Object.values(changesByVersion).map((history, i) => (
