00d4a7431732c47814dd4a63147aa3eff964d789
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2022-04-03 13:36:30 +0200

    chore: upgrade storybook, use swc

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,16 @@
 module.exports = {
   stories: [
     '../packages/components/src/**/*.stories.mdx',
-    '../packages/components/src/**/*.stories.@(js|jsx|ts|tsx)'
+    '../packages/components/src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
+    'storybook-addon-swc',
     '@storybook/addon-links',
-    '@storybook/addon-essentials'
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
 };
